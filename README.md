7c6a89055f98273bc923aee9f32bffb73f9eafd6
skedwards88 <skedwards88@github.com>
2022-12-14 19:28:37 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## TODO
+
+set up github pages and deployment
+set up custom domain
+set up readme
+add where to send inquiry
+add to google maps
+add address to page?
---
+++ preview.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.use(express.static("."));
+
+app.listen(port, () => {
+  console.log(`Preview listening at http://localhost:${port}`);
+});
